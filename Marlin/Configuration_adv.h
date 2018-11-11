a1a663b3175b86d6b74707e8975771205f2440de
Dimitris Kazakos <nemphys@gmail.com>
2018-11-11 17:04:10 +0200

    Disable adaptive step smoothing and enable bevier curve support.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -496,7 +496,7 @@
  * vibration and surface artifacts. The algorithm adapts to provide the best possible step smoothing at the
  * lowest stepping frequencies.
  */
-#define ADAPTIVE_STEP_SMOOTHING
+//#define ADAPTIVE_STEP_SMOOTHING
 
 /**
  * Custom Microstepping
@@ -925,7 +925,7 @@
 #endif
 
 // Support for G5 with XYZE destination and IJPQ offsets. Requires ~2666 bytes.
-//#define BEZIER_CURVE_SUPPORT
+#define BEZIER_CURVE_SUPPORT
 
 // G38.2 and G38.3 Probe Target
 // Set MULTIPLE_PROBING if you want G38 to double touch
