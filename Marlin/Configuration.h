c4f0cb1976c2ee53db2ee51b132720ba00ec4578
Dimitris Kazakos <nemphys@gmail.com>
2018-11-13 18:25:58 +0200

    Decrease Z homing speed while probing.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -816,7 +816,7 @@
 #define XY_PROBE_SPEED 10000
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
+#define Z_PROBE_SPEED_FAST (HOMING_FEEDRATE_Z / 2)
 
 // Feedrate (mm/m) for the "accurate" probe of each point
 #define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
